4320f1c3b3d752d4e1e3c4ba751231efd7eea464
Pavel <pcpvts@gmail.com>
2025-07-03 17:27:42 +0200

    State

--- pokedex/src/App.tsx
+++ pokedex/src/App.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import "./App.css";
+
 import PokemonCard from "./components/PokemonCard";
 
 const pokemonList = [
@@ -9,14 +11,27 @@ const pokemonList = [
   },
   {
     name: "mew",
+    imgSrc: "",
   },
 ];
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("bulbasaur");
+
+  const pokemon = pokemonList.find((pokemon) => pokemon.name === pokemonName);
+
+  if (pokemon == null) {
+    throw new Error("Invalid pokemon name");
+  }
+
   return (
-   <div>
-    <PokemonCard pokemon={pokemonList[0]} />
-   </div>
+    <div>
+      <PokemonCard pokemon={pokemon} />
+      <div>
+        <button onClick={() => setPokemonName("bulbasaur")}>bulbasaur</button>
+        <button onClick={() => setPokemonName("mew")}>mew</button>
+      </div>
+    </div>
   );
 }
 
